6a2640a0ea4e540c178d117eecb386b3025f5e8f
adgcz <adgray@gmail.com>
2020-08-29 14:11:07 +0200

    initial version
    first version

--- README.md
+++ README.md
@@ -1,114 +1,77 @@
-# Create a JavaScript Action
+# Github Project Activity
 
-<p align="center">
-  <a href="https://github.com/actions/javascript-action/actions"><img alt="javscript-action status" src="https://github.com/actions/javascript-action/workflows/units-test/badge.svg"></a>
-</p>
+This action emails a summary of recent project activity in a repo and generates an artifact of the summary
 
-Use this template to bootstrap the creation of a JavaScript action.:rocket:
+## Inputs
 
-This template includes tests, linting, a validation workflow, publishing, and versioning guidance.
+### `token`
 
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
+**Required** Github Token
 
-## Create an action from this template
+### `smtp-server`
 
-Click the `Use this Template` and provide the new repo details for your action
+**Required** SMTP Server
 
-## Code in Main
+### `smtp-server-port`
 
-Install the dependencies
+**Required** SMTP Server Port
 
-```bash
-npm install
-```
+### `auth-user`
 
-Run the tests :heavy_check_mark:
+**Required** Sender user email
 
-```bash
-$ npm test
+### `auth-pwd`
 
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
-...
-```
+**Required** Sender user password
 
-## Change action.yml
+### `email-from`
 
-The action.yml contains defines the inputs and output for your action.
+**Required** Sender from name
 
-Update the action.yml with your name, description, inputs and outputs for your action.
+### `recipient-emails`
 
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
+**Required** Recipient email addresses
 
-## Change the Code
+### `project-numbers`
 
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
+**Required** Project Numbers
 
-```javascript
-const core = require('@actions/core');
-...
+### `days`
 
-async function run() {
-  try {
-      ...
-  }
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
+**Required** Days back
 
-run()
-```
+## Outputs
 
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
+### artifact file with summary in html
 
-## Package for distribution
-
-GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
-
-Actions are run from GitHub repos.  Packaging the action will create a packaged action in the dist folder.
-
-Run prepare
-
-```bash
-npm run prepare
-```
-
-Since the packaged index.js is run from the dist folder.
-
-```bash
-git add dist
-```
-
-## Create a release branch
-
-Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
-
-Checkin to the v1 release branch
-
-```bash
-git checkout -b v1
-git commit -a -m "v1 release"
-```
-
-```bash
-git push origin v1
-```
-
-Your action is now published! :rocket:
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Usage
-
-You can now consume the action by referencing the v1 branch
+## Example usage
 
 ```yaml
-uses: actions/javascript-action@v1
-with:
-  milliseconds: 1000
+on:
+  schedule:
+    # * is a special character in YAML so you have to quote this string
+    - cron:  '1 8 * * 1'
+jobs:
+    testing-project:
+        runs-on: ubuntu-latest
+        steps:
+            - uses: actions/checkout@v2
+            - name: Project changes past 7 days
+              uses: ./.github/actions/project
+              id: project
+              with:
+                  token: ${{ secrets.GITHUB_TOKEN }}
+                  smtp-server: "smtp.gmail.com"
+                  smtp-server-port: "587"
+                  auth-user: ${{ secrets.EMAIL_USER }}
+                  auth-pwd: ${{ secrets.EMAIL_PWD }}
+                  email-from: ${{ secrets.EMAIL_FROM }}
+                  recipient-emails: ${{ secrets.RECIPIENT_EMAILS }}
+                  project-numbers: all
+                  days: 7
+            - name: Upload summary to artifacts
+              uses: actions/upload-artifact@v1
+              with:
+                  name: kanban
+                  path: kanban
 ```
-
-See the [actions tab](https://github.com/actions/javascript-action/actions) for runs of this action! :rocket:
--- index.js
+++ index.js
@@ -1,21 +1,393 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Octokit } = require("@octokit/rest");
+const nodemailer = require("nodemailer");
+const { dirname } = require("path");
+const { existsSync, appendFileSync } = require("fs");
+const makeDir = require("make-dir");
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    try {
+        const token = core.getInput("token");
+        // email inputs
+        const smtpServer = core.getInput("smtp-server");
+        const smtpServerPort = core.getInput("smtp-server-port");
+        const authUser = core.getInput("auth-user"); // secret
+        const authPwd = core.getInput("auth-pwd"); // secret
+        const emailFrom = core.getInput("email-from");
+        const recipientEmails = core.getInput("recipient-emails"); // secret
+        // scope of data
+        const projectNumbers = core.getInput("project-numbers"); // secret
+        const phaseEndDate = core.getInput("phase-end-date"); //
+        const phaseCalendarDays = core.getInput("days"); //
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+        const projectsToQuery = [];
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        if (projectNumbers.toLowerCase() !== "all") {
+            try {
+                projectsToQuery = projectNumbers.split(",");
+            } catch (error) {
+                // invalid project number list
+            }
+        }
+
+        let daysToQuery = 7;
+        if (!isNaN(phaseCalendarDays)) {
+            daysToQuery = parseInt(phaseCalendarDays);
+        }
+
+        const octokit = new Octokit({
+            auth: token,
+            previews: ["starfox-preview", "inertia-preview"],
+        });
+        const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+        // get open projects in repo
+        const repo_projects = await octokit.request(
+            "GET /repos/:owner/:repo/projects",
+            {
+                owner,
+                repo,
+            }
+        );
+
+        // iterate projects in repo
+        const projectKanbans = [];
+        for (let p = 0; p < repo_projects.data.length; p++) {
+            if (
+                projectsToQuery.length === 0 ||
+                projectsToQuery.toLowerCase() === "all" ||
+                projectsToQuery.indexOf(
+                    repo_projects.data[p].number.toString()
+                ) > -1
+            ) {
+                // get columns for project
+                const columns = await octokit.request(
+                    "GET /projects/:project_id/columns",
+                    {
+                        owner,
+                        repo,
+                        project_id: repo_projects.data[p].id,
+                    }
+                );
+
+                // info for summary
+                const kanbanColumns = columns.data.map(function (element) {
+                    return {
+                        name: element.name,
+                        issues: [],
+                    };
+                });
+
+                // iterate columns
+                for (let col = 0; col < columns.data.length; col++) {
+                    // get cards for column
+                    const cards = await octokit.request(
+                        "GET /projects/columns/:column_id/cards",
+                        {
+                            owner,
+                            repo,
+                            column_id: columns.data[col].id,
+                        }
+                    );
+
+                    // iterate cards
+                    for (columnCard in cards.data) {
+                        // get card details
+                        const card = await octokit.request(
+                            "GET /projects/columns/cards/:card_id",
+                            {
+                                owner,
+                                repo,
+                                card_id: cards.data[columnCard].id,
+                            }
+                        );
+
+                        // TODO: notes (currently ignored)
+                        if (typeof card.data.content_url === "string") {
+                            // determine issue number
+                            const contentUrlParts = card.data.content_url.split(
+                                "/"
+                            );
+                            const issue_number =
+                                contentUrlParts[contentUrlParts.length - 1];
+
+                            // GET REPO EVENTS
+                            const issue_events = await octokit.request(
+                                "GET /repos/:owner/:repo/issues/:issue_number/events",
+                                {
+                                    owner,
+                                    repo,
+                                    issue_number,
+                                }
+                            );
+
+                            const events_summary = issue_events.data.map(
+                                (ev) => {
+                                    const Difference_In_Time =
+                                        new Date(ev.created_at).getTime() -
+                                        new Date().getTime();
+                                    const Difference_In_Days = Math.round(
+                                        Difference_In_Time / (1000 * 3600 * 24)
+                                    );
+                                    return {
+                                        event: ev.event,
+                                        days_ago: Difference_In_Days,
+                                        project_card: ev.project_card,
+                                        time_diff: Difference_In_Time,
+                                    };
+                                }
+                            );
+
+                            // sort by time
+                            events_summary.sort((a, b) =>
+                                a.time_diff > b.time_diff ? 1 : -1
+                            );
+
+                            // filter on time period
+                            let columnChange = false;
+                            let newIssue = false;
+                            const relevant_events = [];
+
+                            //TODO: notes added then converted
+                            for (let e = 0; e < events_summary.length; e++) {
+                                // filter out events outside time range
+                                if (
+                                    Math.abs(events_summary[e].days_ago) <=
+                                        daysToQuery &&
+                                    events_summary[e].days_ago <= 0
+                                ) {
+                                    columnChange = true;
+                                    if (
+                                        events_summary[e].event ===
+                                            "added_to_project" ||
+                                        events_summary[e].event ===
+                                            "converted_note_to_issue"
+                                    ) {
+                                        newIssue = true;
+                                    }
+                                    relevant_events.push(events_summary[e]);
+                                }
+                            }
+
+                            // describe flow between now and days ago
+                            const flow = relevant_events
+                                .map((ev, i, arr) => {
+                                    return ev.project_card &&
+                                        ev.project_card.column_name
+                                        ? (i == 0 &&
+                                          ev.project_card.previous_column_name
+                                              ? ev.project_card
+                                                    .previous_column_name +
+                                                " -> "
+                                              : "") +
+                                              ev.project_card.column_name
+                                        : ev.event;
+                                })
+                                .join(" -> ");
+
+                            // get issue details (for title and html url)
+                            const issue = await octokit.request(
+                                "GET /repos/:owner/:repo/issues/:issue_number",
+                                {
+                                    owner,
+                                    repo,
+                                    issue_number,
+                                }
+                            );
+
+                            const issue_comments = await octokit.request(
+                                "/repos/:owner/:repo/issues/:issue_number/comments",
+                                {
+                                    owner,
+                                    repo,
+                                    issue_number,
+                                }
+                            );
+
+                            const total_comments = issue_comments.data.length;
+                            const period_comments = issue_comments.data.filter(
+                                function (comment) {
+                                    const Difference_In_Time =
+                                        new Date(comment.created_at).getTime() -
+                                        new Date().getTime();
+                                    const Difference_In_Days = Math.round(
+                                        Difference_In_Time / (1000 * 3600 * 24)
+                                    );
+                                    return (
+                                        Math.abs(Difference_In_Days) <=
+                                        daysToQuery
+                                    );
+                                }
+                            ).length;
+
+                            // store card for email content
+                            const group = !columnChange ? 0 : !newIssue ? 1 : 2;
+                            kanbanColumns[col].issues.push({
+                                id: issue_number,
+                                html_url: issue.data.html_url,
+                                title: issue.data.title,
+                                group: group,
+                                flow: flow,
+                                total_comments: total_comments,
+                                period_comments: period_comments,
+                            });
+                        } else {
+                            console.log("ignoring note");
+                        }
+                    }
+                    // sort by group (0-unchanged, 1-moved, 2-added)
+                    kanbanColumns[col].issues.sort((a, b) =>
+                        a.group > b.group ? 1 : -1
+                    );
+                }
+
+                projectKanbans.push(
+                    drawKanban(
+                        repo_projects.data[p].name,
+                        kanbanColumns,
+                        daysToQuery
+                    )
+                );
+            }
+        }
+
+        // TODO: can CSS be input?
+        const cssStyle =
+            "<head><style>" +
+            " ul {padding: 12px;} ul li {list-style-type: circle;}" +
+            " .project {overflow-x:auto; text-align: center; } " +
+            " .projectname {font-size:large; font-weight: bold; } " +
+            " a.comments {color:purple; font-style: italic; font-size: small; font-weight: bold;} " +
+            " .grouphead  {font-style: italic; text-align: center; }" +
+            " .grouping0  {background-color: #f0efef;  border-radius: 6px; border: 1px solid #bbbbbb; padding: 8px; }" +
+            " .grouping1  {background-color: #ddeedd;  border-radius: 6px; border: 1px solid #bbbbbb; padding: 8px; }" +
+            " .grouping2  {background-color: #c2d4dd;  border-radius: 6px; border: 1px solid #bbbbbb; padding: 8px; }" +
+            " .column { font-weight: bold; text-align: center;    }" +
+            " table { width: 100%; padding: 4px; border-spacing: 4px;}" +
+            " td {background-color: #f0efef; width:150px; padding: 8px; vertical-align: top; text-align:left; border: 1px solid #cccccc;  border-radius: 6px;} " +
+            " </style><head>";
+
+        // save snapshot as artifact for action run
+        const path = "kanban/index.html";
+        await makeDir(dirname(path));
+        appendFileSync(
+            path,
+            "<html>" +
+                cssStyle +
+                "<body>" +
+                projectKanbans.join("") +
+                "</body></html>"
+        );
+
+        // email kanbans
+        // TODO: validate recipients and other email input
+        if (recipientEmails.indexOf("@") > -1) {
+            let subject =
+                "Projects " +
+                owner +
+                "/" +
+                repo +
+                " - past " +
+                daysToQuery +
+                " days activity";
+            let isTLS = false;
+            const transport = nodemailer.createTransport({
+                host: smtpServer,
+                port: smtpServerPort,
+                secure: isTLS,
+                auth: {
+                    user: authUser,
+                    pass: authPwd,
+                },
+            });
+            const info = await transport.sendMail({
+                from: get_from(emailFrom, authUser),
+                to: recipientEmails,
+                subject: subject,
+                html: cssStyle + projectKanbans.join(""),
+            });
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function get_from(from, username) {
+    if (from.match(/.+<.+@.+>/)) {
+        return from;
+    }
+    return `"${from}" <${username}>`;
+}
+
+function drawKanban(projectName, columns, days_ago) {
+    const today = new Date();
+    const groups = ["No change", "Moved here", "Added"];
+    return (
+        '<br/><div class="project"><span class="projectname">' +
+        projectName +
+        "</span><br/>" +
+        " past " +
+        days_ago +
+        " days activity (as at " +
+        today.getDate() +
+        "/" +
+        (today.getMonth() + 1) +
+        "/" +
+        today.getFullYear() +
+        ")" +
+        "<table><tr>" +
+        columns
+            .map(function (element) {
+                return (
+                    '<td><div class="column">' +
+                    element.name +
+                    "</div>" +
+                    (element.issues.length === 0
+                        ? '<div style="text-align:center;">No issues</div>'
+                        : element.issues
+                              .map((issue, i, arr) => {
+                                  return (
+                                      (i > 0 && arr[i - 1].group !== issue.group
+                                          ? "</div>"
+                                          : "") +
+                                      (i === 0 ||
+                                      arr[i - 1].group !== issue.group
+                                          ? '<br/><div class="grouping' +
+                                            issue.group +
+                                            '"><div class="grouphead">' +
+                                            groups[issue.group] +
+                                            "</div>"
+                                          : "") +
+                                      '<li><a title="' +
+                                      issue.flow +
+                                      '" href="' +
+                                      issue.html_url +
+                                      '" class="group' +
+                                      issue.group +
+                                      '">' +
+                                      issue.title +
+                                      "</a> " +
+                                      (issue.period_comments > 0
+                                          ? ' <a class="comments" title="' +
+                                            issue.period_comments +
+                                            " of " +
+                                            issue.total_comments +
+                                            '" href="' +
+                                            issue.html_url +
+                                            '">new comments</a>'
+                                          : "") +
+                                      "</li>" +
+                                      (i === arr.length - 1 ? "</div>" : "")
+                                  );
+                              })
+                              .join("")) +
+                    "</td>"
+                );
+            })
+            .join("") +
+        "</tr></table></div><br/>"
+    );
 }
 
 run();
